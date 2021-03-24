3ce4d23e67dd7e1030924c846363a9e2fea920f5
Fachrenv89 <fachren71@gmail.com>
2021-03-24 20:36:38 +0700

    init

---
+++ Family.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!-- Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css" />
+
+    <style>
+        body {
+            background-color: #e2edff;
+        }
+
+        .jumbotron {
+            padding-top: 6rem;
+            background-color: #e2edff;
+        }
+
+        a {
+            text-decoration: none;
+            color: #2c3e50;
+        }
+    </style>
+
+    <title>Family</title>
+</head>
+
+<body id="home">
+    <!-- start navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark shadow-sm fixed-top" style="background-color: #2c3e50">
+        <div class="container">
+            <a class="navbar-brand" href="#">FAMILY</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php">HOME</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- end navbar -->
+
+
+    <!-- start jumbotron -->
+    <section class="jumbotron text-center">
+        <img src="assets/img/4.jpg" class="rounded-circle img-thumbnail" alt="" width="200" />
+        <h1 class="display-6"><a href="Ayah.php">Yovic</a></h1>
+        <p class="lead">A handsome father and hard worker </p>
+    </section>
+    <!-- end jumbotron -->
+    <!-- start jumbotron -->
+    <section class="jumbotron text-center">
+        <img src="assets/img/4.jpg" class="rounded-circle img-thumbnail" alt="" width="150" />
+        <h1 class="display-6"><a href="Ibu.php">Henny</a></h1>
+        <p class="lead">A beautiful mother and loves her husband and children</p>
+    </section>
+    <!-- end jumbotron -->
+    <!-- start jumbotron -->
+    <section class="jumbotron text-center">
+        <img src="assets/img/4.jpg" class="rounded-circle img-thumbnail" alt="" width="150" />
+        <h1 class="display-6"><a href="Kakak.php">Faira</a></h1>
+        <p class="lead">My sister is beautiful and smart</p>
+    </section>
+    <!-- end jumbotron -->
+    <!-- start jumbotron -->
+    <section class="jumbotron text-center">
+        <img src="assets/img/1.jpeg" class="rounded-circle img-thumbnail" alt="" width="150" />
+        <h1 class="display-6"><a href="index.php">Fachren</a></h1>
+        <p class="lead">A handsome child, Web dev , Flutter Js, React js</p>
+    </section>
+    <!-- end jumbotron -->
+    <br>
+    <br>
+
+    <!-- start footer -->
+    <footer class="text-success text-center fw-bold">
+        <p>Created with <i class="bi bi-heart-fill text-danger"></i> by <a class="text-primary fw-bold" href="#" style="text-decoration: none">Fachren</a></p>
+    </footer>
+    <!-- end footer -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</body>
+
+</html>
